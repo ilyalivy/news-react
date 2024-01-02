845e4e25495279aee18624c683b3e6b566b389a2
ilyalivy <128998085+ilyalivy@users.noreply.github.com>
2024-01-02 18:09:22 +0200

    fix slider and banners scroll

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,31 +1,36 @@
+import { forwardRef } from 'react';
 import styles from './styles.module.css';
 
-const Categories = ({ categories, selectedCategory, setSelectedCategory }) => {
-    return (
-        <div className={styles.categories}>
-            <button
-                onClick={() => setSelectedCategory(null)}
-                className={!selectedCategory ? styles.active : styles.item}
-            >
-                All
-            </button>
-            {categories.map((category) => {
-                return (
-                    <button
-                        onClick={() => setSelectedCategory(category)}
-                        className={
-                            selectedCategory === category
-                                ? styles.active
-                                : styles.item
-                        }
-                        key={category}
-                    >
-                        {category}
-                    </button>
-                );
-            })}
-        </div>
-    );
-};
+const Categories = forwardRef(
+    ({ categories, selectedCategory, setSelectedCategory }, ref) => {
+        return (
+            <div ref={ref} className={styles.categories}>
+                <button
+                    onClick={() => setSelectedCategory(null)}
+                    className={!selectedCategory ? styles.active : styles.item}
+                >
+                    All
+                </button>
+                {categories.map((category) => {
+                    return (
+                        <button
+                            onClick={() => setSelectedCategory(category)}
+                            className={
+                                selectedCategory === category
+                                    ? styles.active
+                                    : styles.item
+                            }
+                            key={category}
+                        >
+                            {category}
+                        </button>
+                    );
+                })}
+            </div>
+        );
+    }
+);
+
+Categories.displayName = 'Categories';
 
 export default Categories;
