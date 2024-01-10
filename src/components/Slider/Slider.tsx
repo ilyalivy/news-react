aae7eac02b3144af3911c7909b837502f9e5bacf
ilyalivy <128998085+ilyalivy@users.noreply.github.com>
2024-01-10 18:28:10 +0200

    add context and change theme

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -4,23 +4,28 @@ import styles from './styles.module.css';
 interface Props {
     children: React.ReactElement;
     step?: number;
+    isDark: boolean;
 }
 
-const Slider = ({ children, step = 150 }: Props) => {
+const Slider = ({ children, step = 150, isDark }: Props) => {
     const sliderRef = useRef<HTMLElement | null>(null);
 
     const scrollLeft = () => {
-        if (!sliderRef.current) return
+        if (!sliderRef.current) return;
         sliderRef.current.scrollLeft -= step;
     };
 
     const scrollRight = () => {
-        if (!sliderRef.current) return
+        if (!sliderRef.current) return;
         sliderRef.current.scrollLeft += step;
     };
 
     return (
-        <div className={styles.slider}>
+        <div
+            className={`${styles.slider} ${
+                isDark ? styles.dark : styles.light
+            }`}
+        >
             <button onClick={scrollLeft} className={styles.arrow}>
                 {'<'}
             </button>
